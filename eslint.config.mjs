87be9ee128ccc59287ae6d2a490560cc7c11a4af
Petersix <150849737+peterarcuri@users.noreply.github.com>
2025-09-16 01:21:19 -0400

    eslint revised

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,14 @@
-// eslint.config.mjs
-export default [
-  {
-    files: ["**/*.js", "**/*.jsx"],
-    languageOptions: {
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-      },
-    },
-    extends: ["next/core-web-vitals"],
-    rules: {
-      "no-console": "warn",
-      "react/react-in-jsx-scope": "off",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-    },
-  },
-];
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [...compat.extends("next/core-web-vitals")];
+
+export default eslintConfig;
